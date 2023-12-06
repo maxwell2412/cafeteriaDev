900fca53eceac52f466bd35e0c81ed5a25999a6b
maxwell2412 <maxwell24121992@hotmail.com>
2023-12-06 14:30:59 -0300

    nono commit do app!

--- servicos/models.py
+++ servicos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from clientes.models import Cliente
 from .choices import ChoiceCategoriaAdicao
+from datetime import datetime
+from secrets import token_hex
 
 class CategoriaAdicao(models.Model):
 	titulo = models.CharField(max_length=3, choices=ChoiceCategoriaAdicao.choices)
@@ -17,4 +19,23 @@ class Servico(models.Model):
 	data_inicio = models.DateField(null=True)
 	hora_entrega = models.DateTimeField(null=True)
 	finalizado = models.BooleanField(default=False)
-	protecolo = models.CharField(max_length=32, null=True, blank=True)
+	protecolo = models.CharField(max_length=52, null=True, blank=True)
+ 
+	def __str__(self) -> str:
+		return self.titulo
+
+
+	def save(self, *args, **kwargs):
+		if not self.protecolo:
+			self.protecolo = datetime.now().strftime("%D/%M/%Y-%H/%M/%S-") + token_hex(16)
+         
+		super(Servico, self).save(*args, **kwargs)
+
+	def preco_total(self):
+		preco_total = float(0)
+		for categoria in self.categoria_adicao.all():
+			preco_total += float(categoria.preco)
+
+		return preco_total
+
+     
